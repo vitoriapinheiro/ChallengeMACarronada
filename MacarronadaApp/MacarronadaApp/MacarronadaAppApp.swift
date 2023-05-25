c91bc9707ed15e5485f7ffb89884fdfcc90e8f2d
Victor Kammerer <vko@cin.ufpe.br>
2023-05-25 09:03:22 -0300

    notification handler first commit

---
+++ MacarronadaApp/MacarronadaApp/MacarronadaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacarronadaAppApp.swift
+//  MacarronadaApp
+//
+//  Created by vko on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct MacarronadaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
