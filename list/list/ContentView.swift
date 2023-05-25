695dd3a04015cd7dd2a2a5fa01c06ae1e4464166
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-05-25 18:30:06 -0300

    updates

--- list/list/ContentView.swift
+++ list/list/ContentView.swift
@@ -14,32 +14,44 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            TextField("Digite algo", text: $textInput)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-                .onSubmit {
-                    addItemToList()
-                }
-            
             
             VStack {
+                TextField("Digite aqui", text: $textInput)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .onSubmit {
+                        addItemToList()
+                    }
+            }
+            
+            VStack(spacing: 50) {
                 HStack {
                     List(inputList, id: \.self) { input in
-                        Text(input)
+                        HStack {
+                            
+                            Text(input)
+                                .foregroundColor(.black)
+                                .font(.headline)
+                                .padding(10)
+                                .cornerRadius(8)
+                            
+                            TempoView()
+                        }
+                        .listRowBackground(Color.cyan) // Altere a cor de fundo da célula
+                        .frame(height: 87)
+                        // .padding(.vertical, 5)
+                        // .padding(.horizontal, 10)
+                        // .cornerRadius(8)
                     }
-                    
-                    if isTimerViewVisible {
-                        TempoView()
-                    }
-                    
-                    Spacer()
+                   // .frame(width: 375, height: 88)
                 }
+                .background(Color.white)
                 
+                .cornerRadius(8)
                 
+                Spacer()
             }
         }
-        
-        
     }
     
     private func addItemToList() {
@@ -53,76 +65,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct TempoView: View {
-    @State private var timeRemaining = 600 // 10 minutos em segundos
-    @State private var isTimerRunning = false
-    @State private var timer: Timer?
-    
-    var body: some View {
-        HStack {
-            Text(timeString(from: timeRemaining))
-                .font(.body)
-                .padding()
-            
-            ZStack {
-                Circle()
-                    .stroke(Color.gray, lineWidth: 20)
-                
-                Circle()
-                    .trim(from: 0, to: CGFloat(timeRemaining) / 600)
-                    .stroke(Color.blue, lineWidth: 10)
-                    .rotationEffect(.degrees(-90))
-                
-                Button(action: {
-                    if isTimerRunning {
-                        pauseTimer()
-                    } else {
-                        startTimer()
-                    }
-                }) {
-                    Text(isTimerRunning ? Image(systemName: "pause.fill") : Image(systemName: "play.fill")
-                         
-                         
-                    )
-                    .font(.title)
-                }
-            }.frame(width: 20, height: 20)
-            
-            
-            
-        }.frame(width: 100, height: 40)
-            .onAppear {
-                startTimer()
-            }
-    }
-    
-    func startTimer() {
-        if timer == nil {
-            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-                if timeRemaining > 0 {
-                    timeRemaining -= 1
-                } else {
-                    timer?.invalidate()
-                    timer = nil
-                }
-            }
-            isTimerRunning = true
-        }
-    }
-    
-    func pauseTimer() {
-        timer?.invalidate()
-        timer = nil
-        isTimerRunning = false
-    }
-    
-    func timeString(from seconds: Int) -> String {
-        let minutes = seconds / 60
-        let seconds = seconds % 60
-        return String(format: "%02d:%02d", minutes, seconds)
     }
 }
 
--- list/list/TempoView.swift
+++ list/list/TempoView.swift
@@ -1,82 +1,84 @@
-//import SwiftUI
-//
-//struct TempoView: View {
-//    @State private var timeRemaining = 600 // 10 minutos em segundos
-//    @State private var isTimerRunning = false
-//    @State private var timer: Timer?
-//    
-//    var body: some View {
-//        HStack {
-//            Text(timeString(from: timeRemaining))
-//                .font(.largeTitle)
-//                .padding()
-//            
-//            ZStack {
-//                Circle()
-//                    .stroke(Color.gray, lineWidth: 20)
-//                
-//                Circle()
-//                    .trim(from: 0, to: CGFloat(timeRemaining) / 600)
-//                    .stroke(Color.blue, lineWidth: 10)
-//                    .rotationEffect(.degrees(-90))
-//                
-//                Button(action: {
-//                    if isTimerRunning {
-//                        pauseTimer()
-//                    } else {
-//                        startTimer()
-//                    }
-//                }) {
-//                    Text(isTimerRunning ? Image(systemName: "pause.fill") : Image(systemName: "play.fill")
-//                    
-//                    
-//                    )
-//                        .font(.title)
-////                        .padding()
-////                        .background(Color.blue)
-////                        .foregroundColor(.white)
-////                        .cornerRadius(10)
-//                }
-//            }
-//            .frame(width: 200, height: 200)
-//            
-//           
-//        }
-//        .onAppear {
-//            startTimer()
-//        }
-//    }
-//    
-//    func startTimer() {
-//        if timer == nil {
-//            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-//                if timeRemaining > 0 {
-//                    timeRemaining -= 1
-//                } else {
-//                    timer?.invalidate()
-//                    timer = nil
-//                }
-//            }
-//            isTimerRunning = true
-//        }
-//    }
-//    
-//    func pauseTimer() {
-//        timer?.invalidate()
-//        timer = nil
-//        isTimerRunning = false
-//    }
-//    
-//    func timeString(from seconds: Int) -> String {
-//        let minutes = seconds / 60
-//        let seconds = seconds % 60
-//        return String(format: "%02d:%02d", minutes, seconds)
-//    }
-//}
-//
-//struct TempoView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        TempoView()
-//    }
-//}
-//
+
+
+
+
+import SwiftUI
+
+struct TempoView: View {
+    @State private var timeRemaining = 600 // 10 minutos em segundos
+    @State private var isTimerRunning = false
+    @State private var timer: Timer?
+    
+    var body: some View {
+        
+        VStack(alignment: .leading) {
+            
+            Text(timeString(from: timeRemaining))
+                .font(.largeTitle)
+                .foregroundColor(.black)
+            
+            // .frame(width: 50, height: 50)
+            
+            HStack {
+                ProgressView(value: Double(timeRemaining), total: 600)
+                    .accentColor(.black)
+                    .padding(.horizontal)
+                    .frame(width: 326)
+            }
+            
+        }
+        HStack {
+            Button(action: {
+                if isTimerRunning {
+                    pauseTimer()
+                } else {
+                    startTimer()
+                }
+            }) {
+                Image(systemName: isTimerRunning ? "pause.fill" : "play.fill")
+                    .font(.title)
+            }
+            .buttonStyle(BorderlessButtonStyle())
+            .frame(width: 43, height: 88)
+            .background(Color.orange)
+        }
+        
+        
+        .onAppear {
+            startTimer()
+        }
+    }
+    
+    func startTimer() {
+        if timer == nil {
+            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
+                if timeRemaining > 0 {
+                    timeRemaining -= 1
+                } else {
+                    timer?.invalidate()
+                    timer = nil
+                }
+            }
+            isTimerRunning = true
+        }
+    }
+    
+    func pauseTimer() {
+        timer?.invalidate()
+        timer = nil
+        isTimerRunning = false
+    }
+    
+    func timeString(from seconds: Int) -> String {
+        let minutes = seconds / 60
+        let seconds = seconds % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+}
+
+
+struct TempoView_Previews: PreviewProvider {
+    static var previews: some View {
+        TempoView()
+    }
+}
