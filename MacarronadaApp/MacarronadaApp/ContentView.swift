8cf7e667b0100cd6347025a331df8b268d2fccd4
Victor Kammerer <vko@cin.ufpe.br>
2023-05-29 08:20:52 -0300

    notifications

--- MacarronadaApp/MacarronadaApp/ContentView.swift
+++ MacarronadaApp/MacarronadaApp/ContentView.swift
@@ -6,16 +6,55 @@
 //
 
 import SwiftUI
+import UserNotifications
+
+class NotificationManager {
+    
+    static let  instance = NotificationManager() //singleton
+    
+    func requestAuthorization() {
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        UNUserNotificationCenter.current().requestAuthorization(options: options) { (success, error) in
+            if let error = error {
+                print("ERROR: \(error)")
+            } else {
+                print("SUCCESS")
+            }
+        }
+    }
+    
+    func scheduleNotification() {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Seu tempo de atividade encerrou!"
+        content.subtitle = "Aproveite uma pausa para desligar um pouco do projeto."
+        content.sound = .default
+        content.badge = 1
+        
+        //time
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        
+        let request = UNNotificationRequest(
+            identifier: UUID().uuidString,
+            content: content,
+            trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+    }
+}
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            Button("notification") {
+                NotificationManager.instance.requestAuthorization()
+            }
+            Button("Schedule Notification") {
+                NotificationManager.instance.scheduleNotification()
+            }
         }
-        .padding()
+//        .onAppear {
+//            UIApplication.shared.applicationIconBadgeNumber = 0
+//        }
     }
 }
 
@@ -24,3 +63,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+    
